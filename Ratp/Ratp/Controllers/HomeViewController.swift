c1a435a3ddc8179baadc9a7cd741b038ac5ad218
LucasRochette <60231533+LucasRochette@users.noreply.github.com>
2022-07-26 19:24:30 +0200

    swiftproj

---
+++ Ratp/Ratp/Controllers/HomeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HomeViewController.swift
+//  Ratp
+//
+//  Created by Benoit Briatte on 01/06/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func handleFlights(_ sender: Any) {
+        
+    }
+    
+    @IBAction func handleLogin(_ sender: Any) {
+        self.navigationController?.pushViewController(LoginViewController(), animated: true)
+    }
+    
+    @IBAction func handleSignUp(_ sender: Any) {
+        self.navigationController?.pushViewController(SignUpViewController(), animated: true)
+    }
+    
+    
+    @IBAction func handleDatasets() {
+        self.navigationController?.pushViewController(DatasetListViewController(), animated: true)
+    }
+}
